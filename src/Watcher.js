840b2c3aa4734d6d9824f8002607a301e0cfdb65
박현재 <hyunjae.park@navercorp.com>
2018-07-13 01:45:10 +0900

    passive false

--- src/Watcher.js
+++ src/Watcher.js
@@ -65,8 +65,8 @@ export default class Watcher {
 		this._prevPos = rawPos - this.getContainerOffset();
 	}
 	attachEvent() {
-		addEvent(this._view, "scroll", this._onCheck);
-		addEvent(window, "resize", this._onResize);
+		addEvent(this._view, "scroll", this._onCheck, {passive:false});
+		addEvent(window, "resize", this._onResize, {passive:false});
 	}
 	getOrgScrollPos() {
 		return scroll(this._view, this.options.horizontal);
